468c19f226456b0a7251694a9fd0242f458ee9f1
eren ozcan <erenozcandev@gmail.com>
2023-07-03 17:41:30 +0300

    createSlice

---
+++ redux-todo-app/src/redux/todos/todosSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const todosSlice = createSlice({
+  name: 'todos',
+  initialState: {
+    items: [
+      {
+        id: '1',
+        title: 'Learn JavaScript',
+        completed: true
+      },
+      {
+        id: '2',
+        title: 'Learn React',
+        completed: false
+      }
+    ],
+  },
+  reducers: {
+
+  },
+});
+
+export default todosSlice.reducer;
