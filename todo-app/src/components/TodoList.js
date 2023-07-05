749b7e3a37f88f619c0e8327dfe767deef9aa964
eren ozcan <erenozcandev@gmail.com>
2023-07-05 16:40:15 +0300

    delete process & add confirm window etc..

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { toggle } from '../redux/todos/todosSlice'
+import { toggle, destroy } from '../redux/todos/todosSlice'
 
 function TodoList() {
   const dispatch = useDispatch()
   const items = useSelector(state => state.todos.items)
-
+  const handleDestroy = (id) => {
+    if (window.confirm('Are you sure?')) {
+      dispatch(destroy(id));
+    }
+  } 
+  
   return (
     <ul className="todo-list">
       {items.map((item) => (
@@ -13,7 +18,7 @@ function TodoList() {
           <div className="view">
             <input className="toggle" type="checkbox" checked={item.completed} onChange={() => dispatch(toggle({ id: item.id }))} />
             <label>{item.title}</label>
-            <button className="destroy"></button>
+            <button className="destroy" onClick={() => handleDestroy(item.id)}></button>
           </div>
         </li>
       ))}
