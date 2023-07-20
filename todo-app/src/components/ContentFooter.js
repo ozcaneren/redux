392169a7870fb228b6da56da4401af9ffbdb83bf
eren ozcan <erenozcandev@gmail.com>
2023-07-20 13:16:02 +0300

    selector process

--- todo-app/src/components/ContentFooter.js
+++ todo-app/src/components/ContentFooter.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
 import { useSelector, useDispatch } from 'react-redux'
-import { changeActiveFilter, clearCompleted } from '../redux/todos/todosSlice'
+import { changeActiveFilter, clearCompleted, selectTodos } from '../redux/todos/todosSlice'
 
 function ContentFooter() {
   const dispatch = useDispatch()
 
-  const items = useSelector((state) => state.todos.items)
+  const items = useSelector(selectTodos)
   const itemsLeft = items.filter((item) => !item.completed).length
 
   const activeFilter = useSelector((state) => state.todos.activeFilter)
@@ -14,7 +14,7 @@ function ContentFooter() {
   return (
     <footer className="footer">
       <span className="todo-count">
-        <strong>{itemsLeft}</strong> item{itemsLeft > 1 && 's'} left items left
+        <strong>{itemsLeft}</strong> item{itemsLeft > 1 && 's'} left
       </span>
 
       <ul className="filters">
