f8412a5491d6c49fa378ff1f50a0199ab507efa8
eren ozcan <erenozcandev@gmail.com>
2023-07-23 23:07:37 +0300

    get character from api

---
+++ breaking-bad-app/src/pages/Home/index.js
@@ -0,0 +1,21 @@
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchCharacters } from '../../redux/charactersSlice'
+
+function Home() {
+  const data = useSelector((state) => state.characters)
+  const dispatch = useDispatch()
+
+
+  useEffect(() => {
+    dispatch(fetchCharacters())
+  }, [dispatch])
+
+  console.log(data)
+
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
