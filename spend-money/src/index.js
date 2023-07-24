fa3e67235edf9f93be62376aaa8212278841f53f
eren ozcan <erenozcandev@gmail.com>
2023-07-24 20:40:30 +0300

    end

--- spend-money/src/index.js
+++ spend-money/src/index.js
@@ -4,14 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {Provider} from "react-redux"
+import {store} from "./redux/store"
+import {ChakraProvider} from "@chakra-ui/react"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+        <ChakraProvider>
+                 <App />
+       </ChakraProvider>
+    </Provider>
+
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
